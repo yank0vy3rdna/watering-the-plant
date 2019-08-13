f6e6504e28429fbce52b40372ccf8071083d2a68
yank0vy3rdna <yankovyerdna@yandex.ru>
2019-08-13 14:43:07 +0300

    Init repo

---
+++ poliv_fikusa.ino
@@ -0,0 +1,31 @@
+const int OnTimeSecs = 150;
+const int OffTimeSecs = 6*3600-150;
+#define relePin 4
+void setup() {
+  pinMode(relePin, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+void verybigdelay(int timecount) {
+  int countdelays = timecount / 20;
+  int ost = (timecount % 20)*1000;
+  for(int i = 0; i < countdelays; i++){
+    delay(20000);
+  }
+  delay(ost);
+  return;
+}
+bool indicateLed = true;
+void loop()
+{
+  digitalWrite(relePin, LOW);
+  verybigdelay(OnTimeSecs);
+  digitalWrite(relePin, HIGH);
+  verybigdelay(OffTimeSecs);
+  if (indicateLed){
+    indicateLed = false;
+    digitalWrite(13,LOW);
+  }else{
+    indicateLed = true;
+    digitalWrite(13,HIGH);
+  }
+}
